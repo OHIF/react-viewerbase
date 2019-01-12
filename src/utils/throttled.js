236c88327d94c3daf344a3c8f262a0ce9809a9c0
Gustavo Lelis <galelis@gmail.com>
2019-01-12 03:22:06 -0200

    feat(utilFunction): Adding ES6 throttled function

---
+++ src/utils/throttled.js
@@ -0,0 +1,11 @@
+export default function throttled(delay, fn) {
+  let lastCall = 0;
+  return function(...args) {
+    const now = new Date().getTime();
+    if (now - lastCall < delay) {
+      return;
+    }
+    lastCall = now;
+    return fn(...args);
+  };
+}
