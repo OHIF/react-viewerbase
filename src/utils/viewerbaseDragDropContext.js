cf8709273b9041d2fdecb814a282afcf7f2bc025
biharck <biharck@gmail.com>
2019-04-09 10:08:20 -0300

    changing v to V

--- src/utils/viewerbaseDragDropContext.js
+++ src/utils/viewerbaseDragDropContext.js
@@ -3,6 +3,6 @@ import HTML5Backend from 'react-dnd-html5-backend';
 
 // See https://github.com/react-dnd/react-dnd/issues/186#issuecomment-335429067
 // TODO: Find a way for this context to be used in the parent application as well.
-export default function viewerbaseDragDropContext(DecoratedClass) {
+export default function ViewerbaseDragDropContext(DecoratedClass) {
   return DragDropContext(HTML5Backend, null, true)(DecoratedClass);
 }
