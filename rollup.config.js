cf35c33ab683b69b1b116f8f6b34863bb037c3df
dannyrb <danny.ri.brown@gmail.com>
2019-05-29 14:47:16 -0400

    Ditch svgr

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
 import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
+// import svgr from '@svgr/rollup'
 import pkg from './package.json'
 // Deal with https://github.com/rollup/rollup-plugin-commonjs/issues/297
 import builtins from 'rollup-plugin-node-builtins'
@@ -39,7 +39,7 @@ export default {
       modules: false,
     }),
     url(),
-    svgr(),
+    // svgr(),
     babel({
       exclude: 'node_modules/**',
       runtimeHelpers: true,
