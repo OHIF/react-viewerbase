4f2a74693461b74e4e662c5d379fe7aa57bc6089
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2019-06-10 20:48:43 -0300

    fix generalDefaults

--- src/components/userPreferencesModal/__docs__/generalDefaults.js
+++ src/components/userPreferencesModal/__docs__/generalDefaults.js
@@ -1,3 +1,14 @@
 export default {
-  language: 'en-US',
+  currentLanguage: 'en',
+  languages: [
+    {
+      value: 'en',
+      label: 'English',
+    },
+    {
+      value: 'es',
+      label: 'Spanish',
+    },
+  ],
+  onChange: language => {},
 };
