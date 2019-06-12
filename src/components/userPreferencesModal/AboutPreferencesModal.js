c01f6cf45c5c921af0d29f20c4b1be60d2868272
biharck <biharck@gmail.com>
2019-06-12 15:13:14 -0500

    feat(AboutPreferencesModal.js): adding about information
    #529

--- src/components/userPreferencesModal/AboutPreferencesModal.js
+++ src/components/userPreferencesModal/AboutPreferencesModal.js
@@ -8,15 +8,42 @@ import 'react-bootstrap-modal/lib/css/rbm-patch.css';
 import './../../design/styles/common/modal.styl';
 
 export class AboutPreferencesModal extends Component {
+  constructor(props) {
+    super(props);
+  }
   // TODO: Make this component more generic to allow things other than W/L and hotkeys...
   static propTypes = {
     isOpen: PropTypes.bool.isRequired,
     onCancel: PropTypes.func,
   };
 
+  static itemsPreset = [
+    {
+      name: 'Repository URL',
+      value: 'https://github.com/OHIF/Viewers/',
+    },
+    {
+      name: 'Latest Master Commits',
+      value: 'https://github.com/OHIF/Viewers/commits/master',
+    },
+  ];
+
   static defaultProps = {
     isOpen: false,
   };
+
+  renderTableRow(item) {
+    return (
+      <tr key={item.name}>
+        <td>{item.name}</td>
+        <td>
+          <a target="_blank" href={item.value}>
+            {item.value}
+          </a>
+        </td>
+      </tr>
+    );
+  }
 
   render() {
     return (
@@ -33,71 +60,72 @@ export class AboutPreferencesModal extends Component {
           <Modal.Title>About</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div class="modal-body">
-            <div class="p-x-1 p-y-1 m-y-2">
-              <a
-                class="btn btn-default inline"
-                target="_blank"
-                href="https://groups.google.com/forum/#!forum/cornerstone-platform"
-              >
-                Visit the forum
-              </a>
-              <a
-                class="btn btn-default inline"
-                target="_blank"
-                href="https://github.com/OHIF/Viewers/issues"
-              >
-                Report an issue
-              </a>
-            </div>
-            <div class="p-y-1 p-x-1">
-              <h3>Version Information</h3>
-              <table class="table table-responsive">
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Value</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Repository URL</td>
-                    <td>
-                      <a target="_blank" href="https://github.com/OHIF/Viewers">
-                        https://github.com/OHIF/Viewers
-                      </a>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Last Commit Hash</td>
-                    <td>
-                      <a
-                        target="_blank"
-                        href="https://github.com/OHIF/Viewers/commit/"
-                      />
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Latest Tag</td>
-                    <td>
-                      <a
-                        target="_blank"
-                        href="https://github.com/OHIF/Viewers/releases/tag/"
-                      />
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Branch</td>
-                    <td>
-                      <a
-                        target="_blank"
-                        href="https://github.com/OHIF/Viewers/tree/"
-                      />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+          <div>
+            <a
+              class="btn btn-default inline"
+              target="_blank"
+              href="https://groups.google.com/forum/#!forum/cornerstone-platform"
+            >
+              Visit the forum
+            </a>
+            <a
+              class="btn btn-default inline"
+              target="_blank"
+              href="https://github.com/OHIF/Viewers/issues"
+            >
+              Report an issue
+            </a>
+          </div>
+          <div>
+            <h3>Version Information</h3>
+            <table class="table table-responsive">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                {AboutPreferencesModal.itemsPreset.map(item =>
+                  this.renderTableRow(item)
+                )}
+                {/* <tr>
+                  <td>Repository URL</td>
+                  <td>
+                    <a target="_blank" href="https://github.com/OHIF/Viewers">
+                      https://github.com/OHIF/Viewers
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Last Commit Hash</td>
+                  <td>
+                    <a
+                      target="_blank"
+                      href="https://github.com/OHIF/Viewers/commit/"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Latest Tag</td>
+                  <td>
+                    <a
+                      target="_blank"
+                      href="https://github.com/OHIF/Viewers/releases/tag/"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Branch</td>
+                  <td>
+                    <a
+                      target="_blank"
+                      href="https://github.com/OHIF/Viewers/tree/"
+                    />
+                  </td>
+                </tr> */}
+              </tbody>
+            </table>
           </div>
         </Modal.Body>
       </Modal>
